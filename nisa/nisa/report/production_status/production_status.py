2b9858554e52fc671fbe66cb21ef2ee734b69b7b
Resource Factors <it@resourcefactors.com>
2026-02-27 12:34:39 +0400

    Completion Status filter fixed

--- nisa/nisa/report/production_status/production_status.py
+++ nisa/nisa/report/production_status/production_status.py
@@ -22,13 +22,6 @@ def get_columns():
 			"width": 130
 		},
 		{
-			"fieldname": "sales_person",
-			"label": _("Sales Person"),
-			"fieldtype": "Link",
-			"options": "Sales Person",
-			"width": 130
-		},
-		{
 			"fieldname": "delivery_date",
 			"label": _("Delivery Date"),
 			"fieldtype": "Date",
@@ -59,13 +52,6 @@ def get_columns():
 			"label": _("Customer Name"),
 			"fieldtype": "Data",
 			"width": 180
-		},
-		{
-			"fieldname": "sales_person",
-			"label": _("Salesperson"),
-			"fieldtype": "Link",
-			"options": "Sales Person",
-			"width": 150
 		},
 		{
 			"fieldname": "item_code",
@@ -143,7 +129,6 @@ def get_data(filters):
 			pit.received_date,
 			pit.current_assignee,
 			pit.overall_status,
-			(SELECT sales_person FROM `tabSales Team` WHERE parent = pit.sales_order AND parenttype = 'Sales Order' LIMIT 1) as sales_person,
 			(SELECT delivery_date FROM `tabSales Order` WHERE name = pit.sales_order) as delivery_date
 		FROM
 			`tabProduction Item Tracking` pit
@@ -181,6 +166,44 @@ def get_data(filters):
 
 
 def get_not_started_data(filters):
+	pit_conditions = ["AND pit.overall_status = 'Not Started'"]
+	so_conditions = []
+
+	if filters.get("supplier_name"):
+		pit_conditions.append("AND pit.current_assignee = %(supplier_name)s")
+
+	if filters.get("from_date"):
+		pit_conditions.append("AND pit.assigned_date >= %(from_date)s")
+		so_conditions.append("AND so.transaction_date >= %(from_date)s")
+
+	if filters.get("to_date"):
+		pit_conditions.append("AND pit.assigned_date <= %(to_date)s")
+		so_conditions.append("AND so.transaction_date <= %(to_date)s")
+
+	if filters.get("urgent"):
+		pit_conditions.append("AND pit.urgent = 1")
+
+	if filters.get("sales_person"):
+		sp_subquery = """
+			AND pit.sales_order IN (
+				SELECT parent FROM `tabSales Team`
+				WHERE parenttype = 'Sales Order'
+				AND sales_person = %(sales_person)s
+			)
+		"""
+		pit_conditions.append(sp_subquery)
+		so_sp_subquery = """
+			AND so.name IN (
+				SELECT parent FROM `tabSales Team`
+				WHERE parenttype = 'Sales Order'
+				AND sales_person = %(sales_person)s
+			)
+		"""
+		so_conditions.append(so_sp_subquery)
+
+	pit_where = " ".join(pit_conditions)
+	so_where = " ".join(so_conditions)
+
 	# Part A: existing PIT records with no assignee yet
 	part_a = frappe.db.sql("""
 		SELECT
@@ -206,11 +229,12 @@ def get_not_started_data(filters):
 			(SELECT delivery_date FROM `tabSales Order` WHERE name = pit.sales_order) as delivery_date
 		FROM `tabProduction Item Tracking` pit
 		LEFT JOIN `tabSupplier` sup ON pit.current_assignee = sup.name
-		WHERE pit.overall_status = 'Not Started'
+		WHERE 1=1 {pit_where}
 		ORDER BY pit.sales_order, pit.assigned_date
-	""", as_dict=1)
+	""".format(pit_where=pit_where), filters, as_dict=1)
 
 	# Part B: Sales Orders that have NO Production Item Tracking record at all
+	# supplier_name filter is not applicable here (no assignee exists)
 	part_b = frappe.db.sql("""
 		SELECT
 			so.name as sales_order,
@@ -236,12 +260,13 @@ def get_not_started_data(filters):
 		FROM `tabSales Order` so
 		JOIN `tabSales Order Item` soi ON soi.parent = so.name AND soi.parenttype = 'Sales Order'
 		WHERE so.docstatus = 1
+		{so_where}
 		AND NOT EXISTS (
 			SELECT 1 FROM `tabProduction Item Tracking` pit
 			WHERE pit.sales_order = so.name
 		)
 		ORDER BY so.name
-	""", as_dict=1)
+	""".format(so_where=so_where), filters, as_dict=1)
 
 	data = list(part_a) + list(part_b)
 	for row in data:
